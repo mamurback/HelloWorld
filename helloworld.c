efb61a8118fdfb7feaaf296ea359a9f4f2ae1cad
Marco Machado <mamurback@gmail.com>
2014-02-07 22:38:11 -0200

    HelloWorld Melhorias
    Melhoria da função main
    Adição de uma nova função fMsg que imprime a mensagem na tela.

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int fMsg(){
 	printf("Hello World!");
+	return(0);
+}
+
+int main() {
+	/* Chama a função que exibe a msg na tela */
+	fMsg();
     return(0);
 }
